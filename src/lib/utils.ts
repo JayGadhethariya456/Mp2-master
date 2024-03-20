1bb9a60255196276f36a0f08aa73fad0941924b6
Jay <jaykhurana16777@gmail.com>
2024-03-20 10:01:21 +0530

    .

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,7 +9,7 @@ export function absoluteUrl(path: string) {
   if (typeof window !== 'undefined') return path
   if (process.env.VERCEL_URL)
     return `http://${process.env.VERCEL_URL}${path}`
-  return `http://localhost:${
+  return `https://localhost:${
     process.env.PORT ?? 3000
   }${path}`
 }
